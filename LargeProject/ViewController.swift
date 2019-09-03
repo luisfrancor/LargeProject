6152ee2e617e0718f09b5b136e60f969df98fd22
Luis Franco R <luisf.francoreyes@me.com>
2019-09-03 15:32:38 +0200

    Initial Commit

---
+++ LargeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LargeProject
+//
+//  Created by Luis Franco R on 03/09/2019.
+//  Copyright © 2019 Luis Franco R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
